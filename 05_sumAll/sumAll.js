46483696b935070e9e7ac7ce4dc31dd95458f47d
Kenny Laptop <ckennx@gmail.com>
2022-01-08 13:07:43 -0600

    Completed exercises and tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(sumStart, sumEnd) {
+    let sum = 0;
 
+    if ((sumStart > 0 && sumEnd > 0) &&
+     (typeof sumStart == 'number' && typeof sumEnd == 'number')) {
+        if (sumStart > sumEnd) {
+            sum = sumStart;
+
+            for (let i = 0; i < sumStart; i++) {
+                sum+= i;
+            }
+
+        } else {
+            sum = sumEnd;
+
+            for (let i = 0; i < sumEnd; i++) {
+                sum+= i;
+            }
+        }
+
+        return sum;
+    } else {
+        return 'ERROR'
+    }
 };
 
 // Do not edit below this line
