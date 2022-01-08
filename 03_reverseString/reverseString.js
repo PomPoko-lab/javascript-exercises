56f2cc9213e08590094baf08737a0e2a7aff188e
Kenny Laptop <ckennx@gmail.com>
2022-01-08 10:13:28 -0600

    Completed exercises and tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
 const reverseString = function(str) {
+    let combine = [];
 
+    for (let i = 0; i < str.length; i++) {
+        combine.push(str[i]);
+    }
+    
+    combine.reverse();
+
+    return combine.join('');
 };
 
 // Do not edit below this line
