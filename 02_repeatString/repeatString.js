ab48dcc14ca6a62bdf876f2dba4864b27a643e34
Kenny Laptop <ckennx@gmail.com>
2022-01-08 09:40:09 -0600

    Completed exercises and practices

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let returnedString = '';
 
+    if (num < 0) {
+        return 'ERROR';
+    } else {
+        for (let i = 0; i < num; i++) {
+            returnedString += string;
+        }
+    }
+
+    return returnedString;
 };
-
 // Do not edit below this line
 module.exports = repeatString;
