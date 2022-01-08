88df8c12ddb4ebd8b4e2711cd6d878c1e2c73dfa
Kenny Laptop <ckennx@gmail.com>
2022-01-08 13:51:03 -0600

    Completed exercises and tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,22 @@
-const ftoc = function() {
+const ftoc = function(num) {
+  let conversion = (num - 32)/1.8;
+
+  return Math.round(conversion * 10) / 10;
 
 };
 
-const ctof = function() {
+const ctof = function(num) {
+  let conversion = (num * 1.8) + 32;
+
+  return Math.round(conversion * 10) / 10;
 
 };
+
+/* 
+32 fahrenheit to 1 C
+
+rounded to .1
+*/
 
 // Do not edit below this line
 module.exports = {
