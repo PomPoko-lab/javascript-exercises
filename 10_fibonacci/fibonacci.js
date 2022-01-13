e265366c7c61eb2dd9bdda4c2b9e786fd120c5a2
ckennx <ckennx@gmail.com>
2022-01-13 12:32:00 -0600

    Complete Exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = +num;
+    if (num > 0) {
+        let total = [];
+        a = 0;
+        b = 1;
+        for (let index = 1; index <= num; index++) {
+            total.push(a);
+            total.push(b);
+            a += b;
+            b += a;
+        }
+        return total[num];
+    } else {
+        return 'OOPS';
+    }
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
