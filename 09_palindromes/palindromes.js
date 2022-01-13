7755a9a595b0be289f3829474e1694fcbf46f15d
ckennx <ckennx@gmail.com>
2022-01-13 11:24:56 -0600

    Complete exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const removePunctuation = a => {
+    let b = a.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,'');
+    return b.replace(/ /g,'');
+}
 
+const palindromes = string => {
+    const reverseString = Array.from(string).reverse();
+    const combinedString = reverseString
+                            .join('')
+                            .toLowerCase();
+    return removePunctuation(string.toLowerCase()) == removePunctuation(combinedString);
 };
 
 // Do not edit below this line
