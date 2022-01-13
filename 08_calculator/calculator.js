94dd8fb212ef86f0a516d63f81c0c28fd257e0bd
ckennx <ckennx@gmail.com>
2022-01-13 10:31:19 -0600

    Completed exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = (intA, intB) =>  intA + intB;
+
+const subtract = (intA, intB) => intA - intB;
+
+const sum = array => {
+  let total = 0;
+  for (let index = 0; index < array.length; index++) {
+    total += array[index];
+  }
+  return total;
 };
 
-const subtract = function() {
-	
+const multiply = array => {
+  let total = 1;
+  for (let index = 0; index < array.length; index++) {
+    total *= array[index];
+  }
+  return total;
 };
 
-const sum = function() {
-	
-};
+const power = (intA, intB) => intA ** intB;
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+  let total = 1;
+  for (let index = 1; index <= num; index++) {
+    total *= index;
+  }
+  return total;
 };
 
 // Do not edit below this line
